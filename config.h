7f1450ad6effc37a7f257e0e44f90e529932b245
Simo Aleksandrov <simo3003@me.com>
2018-12-06 17:25:26 +0200

    Update font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Misc Tamsyn:style=Bold:pixelsize=18";
+static char *font = "Monaco:pixelsize=17";
 static int borderpx = 16;
 
 /*
